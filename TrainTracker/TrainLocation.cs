162b0875e567da3487b336b56b5954dd6e9e1b10
jdubbeldam <jarkodubbeldam@gmail.com>
2022-08-08 14:38:24 +0200

    first implementation of tracker

---
+++ TrainTracker/TrainLocation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Track;
+using TrainRepository;
+
+namespace TrainTracker {
+  public class TrainLocation {
+    private readonly List<TrackConnection> occupiedConnections;
+
+    public Train Train { get; }
+    public IReadOnlyList<TrackConnection> OccupiedConnections => occupiedConnections;
+    public bool Update(TrackSection changedSection, bool newOccupancy) {
+      if (newOccupancy) {
+        /*
+         * If a section has become occupied:
+         * Check if any of the currently occupied sections is next to
+         * the new section in the direction the train is heading.
+         */
+        foreach(var section in occupiedConnections) {
+          var newSection = section.WalkActiveSections().First();
+          if (newSection.TrackConnectionState == TrackConnection.TrackConnectionIterator.TrackConnectionStateEnum.Active &&
+            newSection.TrackConnection.ViaSection == changedSection) {
+            occupiedConnections.Add(newSection.TrackConnection);
+            return true;
+          }
+        }
+        return false;
+      } else {
+        /*
+         * Else, a section has become unoccupied. If so, check if this
+         * train is currently occupying, and if so, free.
+         */
+        var removedCount = occupiedConnections.RemoveAll(c => c.ViaSection == changedSection);
+        return removedCount > 0;
+      }
+    }
+
+    public override string ToString() {
+      var trainname = Train.Name;
+      var sections = string.Join(", ", OccupiedConnections.Select(x => x.ViaSection.SectionId));
+      return $@"for train {trainname}. 
+
+Sections:
+{sections}";
+    }
+  }
+}
---
+++ TrainTracker/TrainTracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using ReactiveUI;
+using Track;
+
+namespace TrainTracker {
+  public class TrainTracker : IDisposable {
+    private IDisposable subscription;
+    private readonly List<TrainLocation> trainLocations = new List<TrainLocation>();
+
+    public void Setup(TrackRepository trackRepository) {
+      var disposer = new CompositeDisposable();
+      foreach (var section in trackRepository.Sections) {
+        section.WhenAnyValue(x => x.IsOccupied)
+          .DistinctUntilChanged()
+          .Subscribe(newValue => {
+            foreach(var location in trainLocations) {
+              if(location.Update(section, newValue)) {
+                Debug.WriteLine($"Updated occupancy {location}");
+                return;
+              }
+            }
+          })
+          .DisposeWith(disposer);
+      }
+      subscription = disposer;
+    }
+
+
+
+    public void Dispose() => subscription?.Dispose();
+  }
+}
