47fef703ba61c6b78406a052b7c715359048d8c8
jdubbeldam <jarkodubbeldam@gmail.com>
2022-08-10 19:44:58 +0200

    made train reactive...

--- TrainRepository/Train.cs
+++ TrainRepository/Train.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using ReactiveUI;
 using Z21.Domain;
 
 namespace TrainRepository {
-  public class Train : INotifyPropertyChanged {
-
-    public event PropertyChangedEventHandler PropertyChanged;
+  public class Train : ReactiveObject {
+    private TrainSpeed speed;
+    private TrainFunctions functions;
 
     public Train(int address, string name, TrainSpeed speed, TrainFunctions functions) {
       Address = address;
@@ -19,28 +20,22 @@ namespace TrainRepository {
     public int Address { get; }
     public string Name { get; }
 
-    public TrainSpeed Speed { get; private set; }
-    public TrainFunctions Functions { get; private set; }
+    public TrainSpeed Speed { get => speed; private set => this.RaiseAndSetIfChanged(ref speed, value); }
+    public TrainFunctions Functions { get => functions; private set => this.RaiseAndSetIfChanged(ref functions, value); }
 
     public override string ToString() {
       return $"Train {Name}\n{Speed}\n{Functions}";
     }
 
-    public void SetSpeed(TrainSpeed speed) {
-      Speed = speed;
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Speed)));
-    }
+    public void SetSpeed(TrainSpeed speed) => Speed = speed;
 
-    public void SetFunctions(TrainFunctions functions) {
-      Functions = functions;
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Functions)));
-    }
+    public void SetFunctions(TrainFunctions functions) => Functions = functions;
 
     internal void Update(LocomotiveInformation locomotiveInformation) {
-      if(Speed.Equals(locomotiveInformation.TrainSpeed) && Functions == locomotiveInformation.TrainFunctions) { return; }
+      if (Speed.Equals(locomotiveInformation.TrainSpeed) && Functions == locomotiveInformation.TrainFunctions) { return; }
+      using var x = DelayChangeNotifications();
       Speed = locomotiveInformation.TrainSpeed;
       Functions = locomotiveInformation.TrainFunctions;
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("All!"));
     }
   }
 }
