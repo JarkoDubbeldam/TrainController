f3bb716afc1a238caa0284dc212ad4f5f0a6d1af
jdubbeldam <jarkodubbeldam@gmail.com>
2022-08-13 14:07:42 +0200

    lock on location update to ensure it makes sense

--- TrainTracker/TrainLocation.cs
+++ TrainTracker/TrainLocation.cs
@@ -6,6 +6,7 @@ using TrainRepository;
 namespace TrainTracker {
   public class TrainLocation {
     private List<TrackConnection> occupiedConnections;
+    private readonly object _lock = new();
 
     public TrainLocation(Train train, IEnumerable<TrackConnection> trackConnections) {
       occupiedConnections = trackConnections.ToList();
@@ -16,49 +17,51 @@ namespace TrainTracker {
     public IReadOnlyList<TrackConnection> OccupiedConnections => occupiedConnections;
     public bool TrainLost { get; private set; }
     public bool Update(TrackSection changedSection, bool newOccupancy) {
-      if (newOccupancy) {
-        /*
-         * If a section has become occupied:
-         * Check if any of the currently occupied sections is next to
-         * the new section in the direction the train is heading.
-         */
-        foreach (var section in occupiedConnections) {
-          if (section.ViaSection == changedSection) {
-            TrainLost = false;
-            return true;
-          }
-          var newSection = section.WalkActiveSections().First();
-          if (newSection.TrackConnectionState == TrackConnection.TrackConnectionIterator.TrackConnectionStateEnum.Active &&
-            newSection.TrackConnection.ViaSection == changedSection) {
-            occupiedConnections.Add(newSection.TrackConnection);
-            TrainLost = false;
-            return true;
-          }
+      lock (_lock) {
+        if (newOccupancy) {
+          /*
+           * If a section has become occupied:
+           * Check if any of the currently occupied sections is next to
+           * the new section in the direction the train is heading.
+           */
+          foreach (var section in occupiedConnections) {
+            if (section.ViaSection == changedSection) {
+              TrainLost = false;
+              return true;
+            }
+            var newSection = section.WalkActiveSections().First();
+            if (newSection.TrackConnectionState == TrackConnection.TrackConnectionIterator.TrackConnectionStateEnum.Active &&
+              newSection.TrackConnection.ViaSection == changedSection) {
+              occupiedConnections.Add(newSection.TrackConnection);
+              TrainLost = false;
+              return true;
+            }
 
-        }
-        return false;
-      } else {
-        /*
-         * Else, a section has become unoccupied. If so, check if this
-         * train is currently occupying, and if so, free.
-         */
-        var toRemove = new List<TrackConnection>();
-        foreach (var occupiedSection in occupiedConnections) {
-          if (occupiedSection.ViaSection == changedSection) {
-            toRemove.Add(occupiedSection);
           }
-        }
-        /*
-         * If removing would empty the list, set the train to lost instead.
-         */
-        if (toRemove.Count == 0) {
           return false;
-        } else if (occupiedConnections.Count - toRemove.Count == 0) {
-          TrainLost = true;
-          return true;
         } else {
-          occupiedConnections.RemoveAll(c => toRemove.Contains(c));
-          return true;
+          /*
+           * Else, a section has become unoccupied. If so, check if this
+           * train is currently occupying, and if so, free.
+           */
+          var toRemove = new List<TrackConnection>();
+          foreach (var occupiedSection in occupiedConnections) {
+            if (occupiedSection.ViaSection == changedSection) {
+              toRemove.Add(occupiedSection);
+            }
+          }
+          /*
+           * If removing would empty the list, set the train to lost instead.
+           */
+          if (toRemove.Count == 0) {
+            return false;
+          } else if (occupiedConnections.Count - toRemove.Count == 0) {
+            TrainLost = true;
+            return true;
+          } else {
+            occupiedConnections.RemoveAll(c => toRemove.Contains(c));
+            return true;
+          }
         }
       }
     }
