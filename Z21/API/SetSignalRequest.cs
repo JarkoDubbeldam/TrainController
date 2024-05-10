fff71790c126907f9dd883f638db2b45323de47e
jdubbeldam <jarkodubbeldam@gmail.com>
2024-05-10 11:07:35 +0200

    formatting

--- Z21/API/SetSignalRequest.cs
+++ Z21/API/SetSignalRequest.cs
@@ -10,16 +10,16 @@ namespace Z21.API {
     public required SignalMode SignalMode { get; set; }
 
     private static readonly Dictionary<(SignalColour, bool, bool), TurnoutPosition[]> mappings =
-      new Dictionary<(SignalColour, bool, bool), TurnoutPosition[]>{
-        { (SignalColour.Red, false, false), new [] { TurnoutPosition.Position1, TurnoutPosition.Position1, TurnoutPosition.Position1 } },
-        { (SignalColour.Green, false, false), new [] { TurnoutPosition.Position2, TurnoutPosition.Position1, TurnoutPosition.Position1 } },
-        { (SignalColour.Yellow, false, false), new [] { TurnoutPosition.Position1, TurnoutPosition.Position2, TurnoutPosition.Position1 } },
-        { (SignalColour.Green, true, true), new [] { TurnoutPosition.Position2, TurnoutPosition.Position2, TurnoutPosition.Position1 } },
-        { (SignalColour.Yellow, false, true), new [] { TurnoutPosition.Position1, TurnoutPosition.Position1, TurnoutPosition.Position2 } },
-        { (SignalColour.Green, true, false), new [] { TurnoutPosition.Position2, TurnoutPosition.Position1, TurnoutPosition.Position2 } },
-        { (SignalColour.Yellow, true, false), new [] { TurnoutPosition.Position1, TurnoutPosition.Position2, TurnoutPosition.Position2 } },
-        { (SignalColour.Yellow, true, true), new [] { TurnoutPosition.Position2, TurnoutPosition.Position2, TurnoutPosition.Position2 } }
-    };
+      new() {
+        [(SignalColour.Red, false, false)] = [TurnoutPosition.Position1, TurnoutPosition.Position1, TurnoutPosition.Position1],
+        [(SignalColour.Yellow, false, false)] = [TurnoutPosition.Position1, TurnoutPosition.Position2, TurnoutPosition.Position1],
+        [(SignalColour.Yellow, false, true)] = [TurnoutPosition.Position1, TurnoutPosition.Position1, TurnoutPosition.Position2],
+        [(SignalColour.Yellow, true, false)] = [TurnoutPosition.Position1, TurnoutPosition.Position2, TurnoutPosition.Position2],
+        [(SignalColour.Yellow, true, true)] = [TurnoutPosition.Position2, TurnoutPosition.Position2, TurnoutPosition.Position2],
+        [(SignalColour.Green, false, false)] = [TurnoutPosition.Position2, TurnoutPosition.Position1, TurnoutPosition.Position1],
+        [(SignalColour.Green, true, false)] = [TurnoutPosition.Position2, TurnoutPosition.Position1, TurnoutPosition.Position2],
+        [(SignalColour.Green, true, true)] = [TurnoutPosition.Position2, TurnoutPosition.Position2, TurnoutPosition.Position1],
+      };
 
     internal override byte[] ToByteArray() {
       if (!mappings.TryGetValue((SignalMode.SignalColour, SignalMode.Blinking, SignalMode.Number), out var mapping)) {
